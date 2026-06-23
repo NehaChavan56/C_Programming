a41252168c9479cfd8fcef47f155fbde232da479
NehaChavan56 <chavanneha2008@gmail.com>
2026-06-23 19:48:22 +0530

    Add files via upload

---
+++ Competitive/38.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int KMtoMeter(int iNo)
+{
+    return iNo * 1000;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter distance: ");
+    scanf("%d", &iValue);
+
+    iRet = KMtoMeter(iValue);
+
+    printf("Distance in meters is %d", iRet);
+
+    return 0;
+}

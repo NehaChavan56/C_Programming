07b9cc8be8db6582e408c21a6cdd0fdd25ea58fc
NehaChavan56 <chavanneha2008@gmail.com>
2026-06-25 00:07:07 +0530

    Add files via upload

---
+++ Competitive/68.c
@@ -0,0 +1,62 @@
+//Accept N numbers from user and accept one another number as NO,check return index of last occurence of that NO
+#include<stdio.h>
+#include<stdlib.h>
+
+int FirstOcc(int Arr[], int iLength, int iNo)
+{
+    int i = 0;
+
+    for(i = iLength; i >= 0; i--)
+    {
+        if(Arr[i] == iNo)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0, iValue = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iSize);
+
+    printf("Enter the number: ");
+    scanf("%d", &iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element : \n", iCnt+1);
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = FirstOcc(p, iSize, iValue);
+
+    if(iRet == -1)
+    {
+        printf("There is no such number");
+    }
+    else
+    {
+        printf("Last occurence of number is %d",iRet);
+    }
+
+
+    free(p);
+
+    return 0;
+}

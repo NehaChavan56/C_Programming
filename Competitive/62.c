6fc2b6f2e87f80ce985cb46ddb323c0b3ff76639
NehaChavan56 <chavanneha2008@gmail.com>
2026-06-25 00:06:14 +0530

    Add files via upload

---
+++ Competitive/62.c
@@ -0,0 +1,57 @@
+//Accept N numbers from user and return difference between frequency of even numbers and odd numbers
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int Arr[], int iLength)
+{
+    int i, iEvenCount = 0, iOddCount = 0;
+
+    for(i = 0; i < iLength; i++)
+    {
+        if(Arr[i] % 2 == 0)
+        {
+            iEvenCount++;
+        }
+        else
+        {
+            iOddCount++;
+        }
+    }
+
+    return iEvenCount - iOddCount;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements",iSize);
+
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element : %d", iCnt+1);
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Frequency(p, iSize);
+
+    printf("%d", iRet);
+
+    free(p);
+
+    return 0;
+}

1b7cef88c9c00be4ff6c7209920ce490b6d60d10
NehaChavan56 <chavanneha2008@gmail.com>
2026-06-24 23:39:08 +0530

    Add files via upload

---
+++ Competitive/50.c
@@ -0,0 +1,35 @@
+//Write a program which accepts range from user and display all numbers in between that range in reverse order
+
+
+#include<stdio.h>
+
+void RangeDisplayRev(int iStart, int iEnd)
+{
+    int iCnt = 0;
+
+    if(iStart > iEnd)
+    {
+        printf("Invalid range");
+        return;
+    }
+
+    for(iCnt = iEnd; iCnt >= iStart; iCnt--)
+    {    
+        printf("%d ", iCnt);
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter starting point: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter ending point: ");
+    scanf("%d", &iValue2);
+
+    RangeDisplayRev(iValue1, iValue2);
+
+    return 0;
+}

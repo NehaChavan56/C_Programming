23aa6f022012c1362eb1420b7ec1b3ce7ae0287c
NehaChavan56 <chavanneha2008@gmail.com>
2026-07-05 20:17:18 +0530

    Add files via upload

---
+++ Competitive/113.c
@@ -0,0 +1,39 @@
+/*
+input : iRow = 3  iCol = 5
+output: A   A   A   A   A
+        B   B   B   B   B
+        C   C   C   C   C
+       
+*/
+
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+    char ch = '\0';
+
+    for(i = 1,ch = 'A'; i<=iRow ;i++, ch++)
+    {
+        for(j = 1; j <= iCol ;j++)
+        {
+            printf("%c\t",ch);
+        }
+        printf("\n");
+    }
+}      
+
+int main()
+{
+    int iValue1 = 0,iValue2;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns:\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
